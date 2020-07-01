c596006aeeb3f192d559d768424767da66ff116a
Stepan Kozurak <step.kozbvb@gmail.com>
2020-07-01 13:53:16 +0200

    Modified settings and requirements

--- django_mail/settings.py
+++ django_mail/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     }
 }
 
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
